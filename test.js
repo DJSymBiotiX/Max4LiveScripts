2f721c3e8ed16ae0d70dad53c822dfe0ed7ee242
Fernando Chorney <djsbx@djsbx.com>
2016-09-06 21:47:20 -0500

    screwed around with it a bunch, working
    Had to abandon my idea for all key, major/minor strategy until I know more. Going to just make C Major for now.

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var log = function () {
     for (var i in arguments) {
         msg = arguments[i];
         if (msg && msg.toString) {
-            var s = message.toString();
+            var s = msg.toString();
             if (s.indexOf("[object ") >= 0) {
                 s = JSON.stringify(msg);
             }
@@ -34,8 +34,27 @@ log("Reload: ", new Date);
  */
 var play_chord = function (note, offsets) {
 	for (var i in offsets) {
-        offset = offsets[i];
-        outlet(i, note + offset);
+		offset = offsets[i];
+		
+		// Gotta do this because outlet doesn't seem to support
+		// using a variable for the first argument
+		switch (i) {
+			case "0":
+ 				outlet(0, note + offset);
+				break;
+			case "1":
+				outlet(1, note + offset);
+				break;
+			case "2":
+				outlet(2, note + offset);
+				break;
+			case "3":
+				outlet(3, note + offset);
+				break;
+			case "4":
+				outlet(4, note + offset);
+				break;
+		}
     }
 };
 
@@ -43,11 +62,9 @@ var play_chord = function (note, offsets) {
 /*
  * Inlets:
  *  0: Note In
- *  1: Key Signature [C, D, E, F, G, A, B]
- *  2: Major/Minor [Major, Minor]
- *  3-8: C, D, E, F, G, A, B
+ *  1-7: C, D, E, F, G, A, B
  */
-inlets = 9;
+inlets = 8;
 
 /*
  * Outlets:
@@ -55,40 +72,51 @@ inlets = 9;
  */
 outlets = 5;
 
-// Set our objects/arrays for the settings
-
-var all_keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-var key_signature = "C";
-
-var majmin = ["Major", "Minor"];
-var mode = "Major";
-
 // Chords -> Key Signature -> Key Played -> Mode -> Chord
 var chords = {
-    C: {
-        C: {
-            Major: [
-                [0, 4, 7],
-                [0, 4, 7, 11]
-            ],
-            Minor: [
-
-            ]
-        }
-    }
+	C: [
+		[0, 4, 7],		// Triad
+		[0, 4, 7, 11],	// Seventh
+	],
+	D: [
+		[0, 3, 7],		// Triad
+		[0, 3, 7, 10],  // Seventh
+	],
+	E: [
+		[0, 3, 7],		// Triad
+	],
+	F: [
+		[0, 4, 7],		// Triad
+	],
+	G: [
+		[0, 4, 7],		// Triad
+	],
+	A: [
+		[0, 3, 7],		// Triad
+	],
+	B: [
+		[0, 3, 6],		// Triad
+	]
 };
 
-//["Triad", "Seventh", "Add2", "Add4", "Add6", "Add9", "Sus2", "Sus4", "Maj7sys2", "Maj7sus4"]
-var c_key = 0;
+var key_selectors = ["C", "D", "E", "F", "G", "A", "B"];
+var keys = {
+	C: 0, // Triad Seventh Add2 Add4 Add6 Add9 Sus2 Sus4 Maj7sus2 Maj7sus4
+	D: 0, // Triad Seventh Add2 Add4 Add6 Add9 9 Sus2 Sus4 7sus2 7sus4
+	E: 0, // Triad Seventh Add4 Sus4 7sus4
+	F: 0, // Triad Seventh Add2 Add6 Add9 Sus2 Maj7sus2
+	G: 0, // Triad Seventh Add2 Add4 Add6 Add9 9 Sus2 Sus4 7Sus2 7Sus4
+	A: 0, // Triad Seventh Add2 Add4 Add9 9 Sus2 Sus4 7sus2 7sus4
+	B: 0  // Triad Seventh
+};
 
 
 // Determine the key that was played
 var get_key_played = function (note) {
-    var notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
-    for (var i in notes) {
+	var all_keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    for (var i in all_keys) {
         if (note == i || note % 12 == i) {
-            return notes[i];
+            return all_keys[i];
         }
     }
     return -1;
@@ -96,7 +124,11 @@ var get_key_played = function (note) {
 
 var play_note = function (note) {
     var key_played = get_key_played(note);
-    play_chord(note, chords[key_signature][key_played][mode][c_key]);
+
+	// Don't play anything if the key isn't in the key_selectors
+	if (contains(key_selectors, key_played)) {
+    	play_chord(note, chords[key_played][keys[key_played]]);
+	}
 };
 
 var msg_float = function (f) {
@@ -106,20 +138,15 @@ var msg_float = function (f) {
 var msg_int = function (i) {
 	log("Message Int: [", inlet, " : ", i, "]");
 
-	switch (inlet) {
-		case 0: // Note In
-			play_note(i);
-			break;
-		case 1: // Key Signature
-			key_signature = all_keys[i];
-			log ("Key Signature: ", key_signature);
-			break;
-		case 2: // Mode [Major, Minor]
-			mode = majmin[i];
-			log ("Mode: ", mode);
-			break;
-		case 3: // C Key
-			c_key = i;
-			break;
+	if (inlet == 0) {
+		// Note In
+		play_note(i);
+	} else {
+		// Note Selectors
+		keys[key_selectors[inlet - 1]] = i;
+		log(keys);
+		log(key_selectors);
+		log(inlet - 1);
+		log(key_selectors[inlet-1]);
 	}
 }
